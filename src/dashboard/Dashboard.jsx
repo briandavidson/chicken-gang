ba029b2cda587fee31d089e5da67e5ce107376a7
brian davidson <brian.davidson.1981@gmail.com>
2023-06-01 22:58:33 -0400

    added navbar and first draft of chicken places list

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import AuthContext from '../store/auth-context'
+import NavbarComponent from '../navbar/Navbar'
 import './Dashboard.scss';
 
 const DashboardPage = () => {
+  const [chickenPlaces, setChickenPlaces] = React.useState([])
   const authCtx = React.useContext(AuthContext)
+
+  React.useEffect(() => {
+    setChickenPlaces((prev) => {
+      return [{
+        name: 'Honey Wings',
+        score: 6.8
+      }, {
+        name: `My Ex-Wife's Famous Chicken`,
+        score: 6.5
+      }, {
+        name: `Dave's Hot Chicken`,
+        score: 9.7
+      }, {
+        name: `Chicken Shack`,
+        score: 4.2
+      }, {
+        name: `Noori Chicken`,
+        score: 9.5
+      }]
+    })
+  }, [])
 
   const logout = () => {
     authCtx.logout()
@@ -11,10 +34,23 @@ const DashboardPage = () => {
 
   return (
     <>
+      <NavbarComponent/>
       <div className="dashboard">
-        Welcome {authCtx.user?.email}, to the cynerge chicken experience!
+        <div className="chicken-places-list">
+          <div className="column-headers">
+            <span>Place</span>
+            <span>Score</span>
+          </div>
+          <div className="chicken-places">
+            {chickenPlaces.map((place, i) => (
+              <div className="chicken-place" key={i}>
+                <span>{place.name}</span>
+                <span>{place.score}</span>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <button onClick={() => logout()}>logout</button>
     </>
   );
 };
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AuthContext from '../store/auth-context'
+import Avatar from '../assets/images/avatar.png';
+import './Navbar.scss';
+
+const NavbarComponent = () => {
+  const authCtx = React.useContext(AuthContext)
+
+  const logout = () => {
+    authCtx.logout()
+  }
+
+  return (
+    <>
+      <div className="navbar">
+        <span className="navbar-text">The Cynerge Chicken Experience</span>
+        <img src={Avatar} alt="" className="avatar-image" />
+        <span className="logout-button" onClick={() => logout()}>logout</span>
+      </div>
+    </>
+  );
+};
+
+export default NavbarComponent;
